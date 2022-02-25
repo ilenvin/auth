0ae1845936083a6af0ddd990a9ee6dfb2fb3eefc
Ilen Vin <ilenvin@Ilens-MacBook-Pro.local>
2022-02-26 00:42:38 +0700

    [done] register user

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,120 +3,55 @@
   <nav-bar />
   <div class="card">
   <div class="container">
-    <input type="checkbox" id="flip">
-    <div class="cover">
-      <div class="front">
-        <img src="../assets/img/img1.png" alt="">
+    <div class="cover back">
+        <img class="backImg" src="https://i.pinimg.com/originals/2d/0b/35/2d0b351df98ed785b8a61a9374446a81.jpg" alt="background" />
         <div class="text">
           <span class="text-1">Every new friend is a <br> new adventure</span>
           <span class="text-2">Let's get connected</span>
         </div>
-      </div>
-      <div class="back">
-        <img class="backImg" src="../assets/img/img4.jpeg" alt="">
-        <div class="text">
-          <span class="text-1">Complete miles of journey <br> with one step</span>
-          <span class="text-2">Let's get started</span>
-        </div>
-      </div>
     </div>
-    <div class="forms">
-        <div class="form-content">
-          <div class="login-form">
-            <div class="title">Login</div>
-          <form action="#">
-            <div class="input-boxes">
-              <div class="input-box">
-                <i class="fas fa-envelope"></i>
-                <input type="text" placeholder="Enter your email" required>
-              </div>
-              <div class="input-box">
-                <i class="fas fa-lock"></i>
-                <input type="password" placeholder="Enter your password" required>
-              </div>
-              <div class="text"><a href="#">Forgot password?</a></div>
-              <div class="button input-box">
-                <input type="submit" value="Submit">
-              </div>
-              <div class="text sign-up-text">Don't have an account? <label for="flip">Sigup now</label></div>
-            </div>
-        </form>
+    <div class="f-b">
+    <form>
+      <div class="row">
+        <v-icon class="mr-2">mdi-email</v-icon>
+      <v-text-field
+        v-model="email"
+        :error-messages="emailErrors"
+        label="E-mail"
+        required
+        @input="$v.email.$touch()"
+        @blur="$v.email.$touch()"
+      ></v-text-field>
       </div>
-        <div class="signup-form">
-          <div class="title">Signup</div>
-        <form action="#">
-            <div class="input-boxes">
-              <div class="input-box">
-                <i class="fas fa-user"></i>
-                <input type="text" placeholder="Enter your name" required>
-              </div>
-              <div class="input-box">
-                <i class="fas fa-envelope"></i>
-                <input type="text" placeholder="Enter your email" required>
-              </div>
-              <div class="input-box">
-                <i class="fas fa-lock"></i>
-                <input type="password" placeholder="Enter your password" required>
-              </div>
-              <div class="button input-box">
-                <input type="submit" value="Sumbit">
-              </div>
-              <div class="text sign-up-text">Already have an account? <label for="flip">Login now</label></div>
-            </div>
-      </form>
-    </div>
-    </div>
+      <div class="row">
+        <v-icon class="mr-2">mdi-key</v-icon>
+      <v-text-field
+        v-model="password"
+        :error-messages="passwordErrors"
+        label="Password"
+        required
+        @input="$v.password.$touch()"
+        @blur="$v.password.$touch()"
+      ></v-text-field>
+      </div>
+    
+      <v-btn
+        block
+        class="mr-4 mt-8 indigo lighten-3"
+        @click="submit"
+      >
+      Log in
+      </v-btn>
+      <div class="row signup">
+        <span>Don't have an account? </span><br>
+        <router-link to="/signup">
+        <label>Signup now</label>
+        </router-link>
+      </div>
+    </form>
     </div>
   </div>
   </div>
-  <!-- <v-card>
-  <form>
-    <v-text-field
-      v-model="name"
-      :error-messages="nameErrors"
-      :counter="10"
-      label="Name"
-      required
-      @input="$v.name.$touch()"
-      @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-      v-model="email"
-      :error-messages="emailErrors"
-      label="E-mail"
-      required
-      @input="$v.email.$touch()"
-      @blur="$v.email.$touch()"
-    ></v-text-field>
-    <v-select
-      v-model="select"
-      :items="items"
-      :error-messages="selectErrors"
-      label="Item"
-      required
-      @change="$v.select.$touch()"
-      @blur="$v.select.$touch()"
-    ></v-select>
-    <v-checkbox
-      v-model="checkbox"
-      :error-messages="checkboxErrors"
-      label="Do you agree?"
-      required
-      @change="$v.checkbox.$touch()"
-      @blur="$v.checkbox.$touch()"
-    ></v-checkbox> 
-
-    <v-btn
-      class="mr-4"
-      @click="submit"
-    >
-      submit
-    </v-btn>
-    <v-btn @click="clear">
-      clear
-    </v-btn>
-  </form>
-  </v-card> -->
   <Footer />
   </div>
 </template>
@@ -125,7 +60,7 @@
 import NavBar from '../components/NavBar.vue'
 import Footer from '../components/Footer.vue'
 import { validationMixin } from 'vuelidate'
-import { required, maxLength, email } from 'vuelidate/lib/validators'
+import { required, email } from 'vuelidate/lib/validators'
 
 export default {
   name: 'LogIn',
@@ -135,54 +70,28 @@ export default {
     Footer
   },
     validations: {
-        name: { required, maxLength: maxLength(10) },
         email: { required, email },
-        select: { required },
-        checkbox: {
-        checked (val) {
-            return val
-        },
-        },
+        password: { required }
     },
 
     data: () => ({
-        name: '',
         email: '',
-        // select: null,
-        // items: [
-        // 'Item 1',
-        // 'Item 2',
-        // 'Item 3',
-        // 'Item 4',
-        // ],
-        // checkbox: false,
+        password: ''
     }),
 
     computed: {
-        // checkboxErrors () {
-        // const errors = []
-        // if (!this.$v.checkbox.$dirty) return errors
-        // !this.$v.checkbox.checked && errors.push('You must agree to continue!')
-        // return errors
-        // },
-        // selectErrors () {
-        // const errors = []
-        // if (!this.$v.select.$dirty) return errors
-        // !this.$v.select.required && errors.push('Item is required')
-        // return errors
-        // },
-        nameErrors () {
-        const errors = []
-        if (!this.$v.name.$dirty) return errors
-        !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
-        !this.$v.name.required && errors.push('Name is required.')
-        return errors
-        },
         emailErrors () {
         const errors = []
         if (!this.$v.email.$dirty) return errors
         !this.$v.email.email && errors.push('Must be valid e-mail')
         !this.$v.email.required && errors.push('E-mail is required')
+        return errors
+        },
+        passwordErrors () {
+        const errors = []
+        if (!this.$v.password.$dirty) return errors
+        // !this.$v.password.minLength && errors.push('Password must be minimum of 8 characters')
+        !this.$v.password.required && errors.push('Password is required')
         return errors
         },
     },
@@ -191,13 +100,6 @@ export default {
         submit () {
         this.$v.$touch()
         },
-        clear () {
-        this.$v.$reset()
-        this.name = ''
-        this.email = ''
-        this.select = null
-        this.checkbox = false
-        },
     },
 
 }
@@ -205,87 +107,69 @@ export default {
 <style scoped>
 /* Google Font Link */
 @import 'https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap';
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Poppins" , sans-serif;
-  position: center;
+* {
+  display: inline;
+  font-family: "Poppins", sans-serif;
+}
+.row {
+  display: flex;
+  justify-content: flex-start;
 }
 .card{
   min-height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
+  /* position: fixed; */
   background: #ffffff;
   padding: 30px;
 }
-body{
-  min-height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: #7d2ae8;
-  padding: 30px;
-}
+
 .container{
   position: relative;
+  display: flex;
+  justify-content: flex-end;
   max-width: 850px;
   width: 100%;
   background: #fff;
-  padding: 40px 30px;
+  padding: 30px 30px;
   box-shadow: 0 5px 10px rgba(0,0,0,0.2);
   perspective: 2700px;
 }
-.container .cover{
+.cover {
   position: absolute;
   top: 0;
-  left: 50%;
+  right: 50%;
   height: 100%;
   width: 50%;
-  z-index: 98;
-  transition: all 1s ease;
-  transform-origin: left;
-  transform-style: preserve-3d;
 }
-.container #flip:checked ~ .cover{
-  transform: rotateY(-180deg);
-}
- .container .cover .front,
- .container .cover .back{
-  position: absolute;
-  top: 0;
-  left: 0;
+.f-b{
   height: 100%;
-  width: 100%;
+  width: 50%;
+  padding: 2em 2em;
+  padding-left: 3em;
 }
-.cover .back{
-  transform: rotateY(180deg);
-  backface-visibility: hidden;
-}
-.container .cover::before,
-.container .cover::after{
-  content: '';
-  position: absolute;
+.signup{
+  /* width: 50%; */
   height: 100%;
-  width: 100%;
-  background: #7d2ae8;
-  opacity: 0.5;
-  z-index: 12;
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  text-align: center;
+  padding: 2em;
 }
-.container .cover::after{
-  opacity: 0.3;
-  transform: rotateY(180deg);
-  backface-visibility: hidden;
+label{
+  color: #9FA8DA;
+  cursor: pointer;
 }
-.container .cover img{
+.backImg{
   position: absolute;
   height: 100%;
   width: 100%;
   object-fit: cover;
-  z-index: 10;
+  opacity: 0.7;
 }
-.container .cover .text{
+.text{
   position: absolute;
   z-index: 130;
   height: 100%;
@@ -295,137 +179,11 @@ body{
   align-items: center;
   justify-content: center;
 }
-.cover .text .text-1,
-.cover .text .text-2{
+.text .text-1,
+.text .text-2{
   font-size: 26px;
   font-weight: 600;
   color: #fff;
   text-align: center;
-}
-.cover .text .text-2{
-  font-size: 15px;
-  font-weight: 500;
-}
-.container .forms{
-  height: 100%;
-  width: 100%;
-  background: #fff;
-}
-.container .form-content{
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-.form-content .login-form,
-.form-content .signup-form{
-  width: calc(100% / 2 - 25px);
-}
-.forms .form-content .title{
-  position: relative;
-  font-size: 24px;
-  font-weight: 500;
-  color: #333;
-}
-.forms .form-content .title:before{
-  content: '';
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  height: 3px;
-  width: 25px;
-  background: #7d2ae8;
-}
-.forms .signup-form  .title:before{
-  width: 20px;
-}
-.forms .form-content .input-boxes{
-  margin-top: 30px;
-}
-.forms .form-content .input-box{
-  display: flex;
-  align-items: center;
-  height: 50px;
-  width: 100%;
-  margin: 10px 0;
-  position: relative;
-}
-.form-content .input-box input{
-  height: 100%;
-  width: 100%;
-  outline: none;
-  border: none;
-  padding: 0 30px;
-  font-size: 16px;
-  font-weight: 500;
-  border-bottom: 2px solid rgba(0,0,0,0.2);
-  transition: all 0.3s ease;
-}
-.form-content .input-box input:focus,
-.form-content .input-box input:valid{
-  border-color: #7d2ae8;
-}
-.form-content .input-box i{
-  position: absolute;
-  color: #7d2ae8;
-  font-size: 17px;
-}
-.forms .form-content .text{
-  font-size: 14px;
-  font-weight: 500;
-  color: #333;
-}
-.forms .form-content .text a{
-  text-decoration: none;
-}
-.forms .form-content .text a:hover{
-  text-decoration: underline;
-}
-.forms .form-content .button{
-  color: #fff;
-  margin-top: 40px;
-}
-.forms .form-content .button input{
-  color: #fff;
-  background: #7d2ae8;
-  border-radius: 6px;
-  padding: 0;
-  cursor: pointer;
-  transition: all 0.4s ease;
-}
-.forms .form-content .button input:hover{
-  background: #5b13b9;
-}
-.forms .form-content label{
-  color: #5b13b9;
-  cursor: pointer;
-}
-.forms .form-content label:hover{
-  text-decoration: underline;
-}
-.forms .form-content .login-text,
-.forms .form-content .sign-up-text{
-  text-align: center;
-  margin-top: 25px;
-}
-.container #flip{
-  display: none;
-}
-@media (max-width: 730px) {
-  .container .cover{
-    display: none;
-  }
-  .form-content .login-form,
-  .form-content .signup-form{
-    width: 100%;
-  }
-  .form-content .signup-form{
-    display: none;
-  }
-  .container #flip:checked ~ .forms .signup-form{
-    display: block;
-  }
-  .container #flip:checked ~ .forms .login-form{
-    display: none;
-  }
 }
 </style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,99 +3,71 @@
   <nav-bar />
   <div class="card">
   <div class="container">
-    <input type="checkbox" id="flip">
-    <div class="cover">
-      <div class="back">
-        <img class="backImg" src="../assets/img/img4.jpeg" alt="">
+    <div class="f-b">
+    <form @submit.prevent="registerUser">
+      <div class="row">
+      <v-icon class="mr-2">mdi-account</v-icon>
+      <v-text-field
+        v-model="name"
+        :error-messages="nameErrors"
+        :counter="10"
+        label="Name"
+        required
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>
+      </div>
+      <div class="row">
+        <v-icon class="mr-2">mdi-email</v-icon>
+      <v-text-field
+        v-model="email"
+        :error-messages="emailErrors"
+        label="E-mail"
+        required
+        @input="$v.email.$touch()"
+        @blur="$v.email.$touch()"
+      ></v-text-field>
+      </div>
+      <div class="row">
+        <v-icon class="mr-2">mdi-key</v-icon>
+      <v-text-field
+        v-model="password"
+        :error-messages="passwordErrors"
+        label="Password"
+        required
+        @input="$v.password.$touch()"
+        @blur="$v.password.$touch()"
+      ></v-text-field>
+      </div>
+    
+      <v-btn
+        block
+        class="mr-4 mt-8 indigo lighten-3"
+        type="submit"
+      >
+        submit
+      </v-btn>
+    </form>
+    </div>
+    <div class="cover back">
+        <img class="backImg" src="https://i.pinimg.com/originals/45/5b/0c/455b0c8f69ca0c63f09301e71f98880e.jpg" alt="background" />
         <div class="text">
           <span class="text-1">Complete miles of journey <br> with one step</span>
           <span class="text-2">Let's get started</span>
         </div>
       </div>
-    </div>
-   
-    <div class="signup-form">
-        <div class="title">Sign up</div>
-    <form action="#">
-        <div class="input-boxes">
-            <div class="input-box">
-            <i class="fas fa-user"></i>
-            <input type="text" placeholder="Enter your name" required>
-            </div>
-            <div class="input-box">
-            <i class="fas fa-envelope"></i>
-            <input type="text" placeholder="Enter your email" required>
-            </div>
-            <div class="input-box">
-            <i class="fas fa-lock"></i>
-            <input type="password" placeholder="Enter your password" required>
-            </div>
-            <div class="button input-box">
-            <input type="submit" value="Sumbit">
-            </div>
-            <div class="text sign-up-text">Already have an account? <label for="flip">Log in now</label></div>
-        </div>
-      </form>
-    </div>
-    </div>
   </div>
-  <!-- <v-card>
-  <form>
-    <v-text-field
-      v-model="name"
-      :error-messages="nameErrors"
-      :counter="10"
-      label="Name"
-      required
-      @input="$v.name.$touch()"
-      @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-      v-model="email"
-      :error-messages="emailErrors"
-      label="E-mail"
-      required
-      @input="$v.email.$touch()"
-      @blur="$v.email.$touch()"
-    ></v-text-field>
-    <v-select
-      v-model="select"
-      :items="items"
-      :error-messages="selectErrors"
-      label="Item"
-      required
-      @change="$v.select.$touch()"
-      @blur="$v.select.$touch()"
-    ></v-select>
-    <v-checkbox
-      v-model="checkbox"
-      :error-messages="checkboxErrors"
-      label="Do you agree?"
-      required
-      @change="$v.checkbox.$touch()"
-      @blur="$v.checkbox.$touch()"
-    ></v-checkbox> 
-
-    <v-btn
-      class="mr-4"
-      @click="submit"
-    >
-      submit
-    </v-btn>
-    <v-btn @click="clear">
-      clear
-    </v-btn>
-  </form>
-  </v-card> -->
+  </div>
   <Footer />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import NavBar from '../components/NavBar.vue'
 import Footer from '../components/Footer.vue'
 import { validationMixin } from 'vuelidate'
-import { required, maxLength, email, password } from 'vuelidate/lib/validators'
+import { required, maxLength, email, minLength } from 'vuelidate/lib/validators'
 
 export default {
   name: 'SignUp',
@@ -107,13 +79,7 @@ export default {
     validations: {
         name: { required, maxLength: maxLength(10) },
         email: { required, email },
-        password: {required, password},
-        select: { required },
-        checkbox: {
-        checked (val) {
-            return val
-        },
-        },
+        password: {required, minLength: minLength(8) },
     },
 
     data: () => ({
@@ -137,107 +103,104 @@ export default {
         !this.$v.email.required && errors.push('E-mail is required')
         return errors
         },
+        passwordErrors () {
+        const errors = []
+        if (!this.$v.password.$dirty) return errors
+        !this.$v.password.minLength && errors.push('Password must be minimum of 8 characters')
+        !this.$v.password.required && errors.push('Password is required')
+        return errors
+        },
     },
 
     methods: {
-        submit () {
-        this.$v.$touch()
+        // submit () {
+        // this.$v.$touch()
+
+        // },
+        async registerUser() {
+          this.$v.$touch()
+          // this.isApierror = false;
+          // this.isSuccess = false;
+          if (!this.$v.$invalid) {
+            const postData = {
+            name: this.name,
+            email: this.email,
+            password: this.password,
+            };
+
+            await axios.post("http://localhost:8000/api/register", postData)
+              .then(res => {
+                  console.log(res);
+                  alert('register sucess');
+                  this.$router.push({ name: 'Home' })
+                  // this.isSuccess = true;
+                  // this.isApierror = false;
+                  // this.$v.$reset();
+                  // this.resetData();
+              })
+              .catch(err => {
+                  // this.errors = err.response.data
+                  console.log(err.response.data)
+              })      
+            }
+          }
         },
-        clear () {
-        this.$v.$reset()
-        this.name = ''
-        this.email = ''
-        this.select = null
-        this.checkbox = false
-        },
-    },
 
 }
 </script>
 <style scoped>
 /* Google Font Link */
 @import 'https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap';
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Poppins" , sans-serif;
-  position: center;
+* {
+  display: inline;
+  font-family: "Poppins", sans-serif;
+}
+.row {
+  display: flex;
+  justify-content: flex-start;
 }
 .card{
   min-height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
+  /* position: fixed; */
   background: #ffffff;
   padding: 30px;
 }
-body{
-  min-height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: #7d2ae8;
-  padding: 30px;
-}
+
 .container{
   position: relative;
+  display: flex;
+  justify-content: flex-start;
   max-width: 850px;
   width: 100%;
   background: #fff;
-  padding: 40px 30px;
+  padding: 30px 30px;
   box-shadow: 0 5px 10px rgba(0,0,0,0.2);
   perspective: 2700px;
 }
-.container .cover{
+.f-b{
+  height: 100%;
+  width: 50%;
+  padding: 2em 2em;
+  padding-right: 4em;
+}
+.cover {
   position: absolute;
   top: 0;
   left: 50%;
   height: 100%;
   width: 50%;
-  z-index: 98;
-  transition: all 1s ease;
-  transform-origin: left;
-  transform-style: preserve-3d;
 }
-.container #flip:checked ~ .cover{
-  transform: rotateY(-180deg);
-}
- .container .cover .front,
- .container .cover .back{
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-}
-.cover .back{
-  transform: rotateY(180deg);
-  backface-visibility: hidden;
-}
-.container .cover::before,
-.container .cover::after{
-  content: '';
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  background: #7d2ae8;
-  opacity: 0.5;
-  z-index: 12;
-}
-.container .cover::after{
-  opacity: 0.3;
-  transform: rotateY(180deg);
-  backface-visibility: hidden;
-}
-.container .cover img{
+.backImg{
   position: absolute;
   height: 100%;
   width: 100%;
   object-fit: cover;
-  z-index: 10;
+  opacity: 0.7;
 }
-.container .cover .text{
+.text{
   position: absolute;
   z-index: 130;
   height: 100%;
@@ -247,137 +210,11 @@ body{
   align-items: center;
   justify-content: center;
 }
-.cover .text .text-1,
-.cover .text .text-2{
+.text .text-1,
+.text .text-2{
   font-size: 26px;
   font-weight: 600;
   color: #fff;
   text-align: center;
-}
-.cover .text .text-2{
-  font-size: 15px;
-  font-weight: 500;
-}
-.container .forms{
-  height: 100%;
-  width: 100%;
-  background: #fff;
-}
-.container .form-content{
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-.form-content .login-form,
-.form-content .signup-form{
-  width: calc(100% / 2 - 25px);
-}
-.forms .form-content .title{
-  position: relative;
-  font-size: 24px;
-  font-weight: 500;
-  color: #333;
-}
-.forms .form-content .title:before{
-  content: '';
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  height: 3px;
-  width: 25px;
-  background: #7d2ae8;
-}
-.forms .signup-form  .title:before{
-  width: 20px;
-}
-.forms .form-content .input-boxes{
-  margin-top: 30px;
-}
-.forms .form-content .input-box{
-  display: flex;
-  align-items: center;
-  height: 50px;
-  width: 100%;
-  margin: 10px 0;
-  position: relative;
-}
-.form-content .input-box input{
-  height: 100%;
-  width: 100%;
-  outline: none;
-  border: none;
-  padding: 0 30px;
-  font-size: 16px;
-  font-weight: 500;
-  border-bottom: 2px solid rgba(0,0,0,0.2);
-  transition: all 0.3s ease;
-}
-.form-content .input-box input:focus,
-.form-content .input-box input:valid{
-  border-color: #7d2ae8;
-}
-.form-content .input-box i{
-  position: absolute;
-  color: #7d2ae8;
-  font-size: 17px;
-}
-.forms .form-content .text{
-  font-size: 14px;
-  font-weight: 500;
-  color: #333;
-}
-.forms .form-content .text a{
-  text-decoration: none;
-}
-.forms .form-content .text a:hover{
-  text-decoration: underline;
-}
-.forms .form-content .button{
-  color: #fff;
-  margin-top: 40px;
-}
-.forms .form-content .button input{
-  color: #fff;
-  background: #7d2ae8;
-  border-radius: 6px;
-  padding: 0;
-  cursor: pointer;
-  transition: all 0.4s ease;
-}
-.forms .form-content .button input:hover{
-  background: #5b13b9;
-}
-.forms .form-content label{
-  color: #5b13b9;
-  cursor: pointer;
-}
-.forms .form-content label:hover{
-  text-decoration: underline;
-}
-.forms .form-content .login-text,
-.forms .form-content .sign-up-text{
-  text-align: center;
-  margin-top: 25px;
-}
-.container #flip{
-  display: none;
-}
-@media (max-width: 730px) {
-  .container .cover{
-    display: none;
-  }
-  .form-content .login-form,
-  .form-content .signup-form{
-    width: 100%;
-  }
-  .form-content .signup-form{
-    display: none;
-  }
-  .container #flip:checked ~ .forms .signup-form{
-    display: block;
-  }
-  .container #flip:checked ~ .forms .login-form{
-    display: none;
-  }
 }
 </style>
